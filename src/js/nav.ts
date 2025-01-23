2212140fe7605cb7f7045fd53a35f2e676eda056
SUHO PARK <duglikespiano@gmail.com>
2025-01-24 03:04:25 +0900

    REAFACTOR - NAV AND LINK CODES REFACTORED

---
+++ src/js/nav.ts
@@ -0,0 +1,6 @@
+const navLinks = document.querySelectorAll('[data-navLink');
+navLinks.forEach((navLink) => {
+	if (navLink.getAttribute('href') === window.location.pathname) {
+		navLink.setAttribute('aria-current', 'page');
+	}
+});

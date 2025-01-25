d49510cda894b9f09b27c21d346d9788165bd509
SUHO PARK <duglikespiano@gmail.com>
2025-01-26 01:00:41 +0900

    FEATURE - 404PAGE ADDED

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon()],
+	integrations: [
+		icon({
+			include: {
+				tabler: ['*'],
+			},
+		}),
+	],
 });

4f203ad52ce40a4214146fefc7712b082843e3b1
SUHO PARK <duglikespiano@gmail.com>
2025-02-02 16:18:56 +0900

    FEATURE - SORT POSTS FEATURE ADDED

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -14,3 +14,27 @@ export function formatDate(date: Date) {
 		timeZone: 'UTC',
 	});
 }
+
+export function formatBlogPosts(
+	posts: any,
+	{ filterOutDrafts = true, filterOutFuturePosts = true, sortByDate = true, limit = 10 } = {}
+) {
+	const filteredPosts = posts.reduce((acc: any, post: any) => {
+		const { date, draft } = post.frontmatter;
+		if (filterOutDrafts && draft) return acc;
+		if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+		acc.push(post);
+		return acc;
+	}, []);
+
+	if (sortByDate) {
+		filteredPosts.sort((a: any, b: any) => new Date(b.frontmatter.date).getTime() - new Date(a.frontmatter.date).getTime());
+	} else {
+		filteredPosts.sort(() => Math.random() - 0.5);
+	}
+
+	if (typeof limit === 'number') {
+		return filteredPosts.slice(0, limit);
+	}
+	return filteredPosts;
+}

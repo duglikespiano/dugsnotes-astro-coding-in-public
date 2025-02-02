3e1e9aa06d5fad5cbd719663626bf14dd421e178
SUHO PARK <duglikespiano@gmail.com>
2025-02-03 00:58:14 +0900

    SETTINGS - SETTING FOR INTEGRATIONS ADDED

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,18 @@ import icon from 'astro-icon';
 
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.test.com',
-	integrations: [
-		icon({
-			include: {
-				tabler: ['*'],
-			},
-		}),
-		sitemap({ filter: (page) => page !== 'https://www.test.com/test' }),
-	],
+    site: 'https://www.test.com',
+    integrations: [icon({
+        include: {
+            tabler: ['*'],
+        },
+        }), sitemap({ filter: (page) => page !== 'https://www.test.com/test' }), react(), tailwind(), partytown()],
 });

8199fdbe802caa35771b40ac3798e2e121293f1c
SUHO PARK <duglikespiano@gmail.com>
2025-02-02 20:31:41 +0900

    FEATURE - SITEMAP ADDED

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://www.test.com',
 	integrations: [
 		icon({
 			include: {
 				tabler: ['*'],
 			},
 		}),
+		sitemap({ filter: (page) => page !== 'https://www.test.com/test' }),
 	],
 });
